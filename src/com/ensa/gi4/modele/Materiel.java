0507a9dcc7cbbe91370f204a112531db81aa7e27
malika20-00 <96023132+malika20-00@users.noreply.github.com>
2022-04-02 00:02:22 +0000

    ee

--- src/com/ensa/gi4/modele/Materiel.java
+++ src/com/ensa/gi4/modele/Materiel.java
@@ -2,6 +2,7 @@ package com.ensa.gi4.modele;
 
 public abstract class Materiel {
     private String name;
+    private int id;
 
     public String getName() {
         return name;
@@ -10,4 +11,12 @@ public abstract class Materiel {
     public void setName(String name) {
         this.name = name;
     }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
 }
