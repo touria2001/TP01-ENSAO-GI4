9a0dfddb11ab7297a7747d9876ec4364bc4fc475
touria2001 <77863089+touria2001@users.noreply.github.com>
2022-04-02 00:37:24 +0000

    llgreg

--- src/com/ensa/gi4/datatabase/Factory.java
+++ src/com/ensa/gi4/datatabase/Factory.java
@@ -1,6 +1,38 @@
 package com.ensa.gi4.datatabase;
 
+package com.ensa.gi4.datatabase;
+
+import com.ensa.gi4.modele.Chaise;
+import com.ensa.gi4.modele.Livre;
+import com.ensa.gi4.modele.Materiel;
+import com.ensa.gi4.service.api.GestionMaterielService;
+import com.ensa.gi4.service.impl.GestionMaterielServiceImpl;
+import org.springframework.context.support.LiveBeansView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Factory {
 
-	
+	  private List<Materiel> listMateriel = new ArrayList<>();
+
+	    public Factory()
+	    {
+	        Chaise chaise = new Chaise();
+	        chaise.setName("sofa");
+	        Livre livre = new Livre();
+	        livre.setName("math livre");
+
+	        listMateriel.add(chaise);
+	        listMateriel.add(livre);
+	    }
+
+	    public List<Materiel> getListMateriel() {
+	        return listMateriel;
+	    }
+
+	    public void setListMateriel(List<Materiel> listMateriel) {
+	        this.listMateriel = listMateriel;
+	    }
+
 }
