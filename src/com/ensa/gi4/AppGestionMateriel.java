b2bdb12a929794656cffcb443671f2c6f02ac18d
malika20-00 <96023132+malika20-00@users.noreply.github.com>
2022-04-02 01:55:04 +0000

    ff

--- src/com/ensa/gi4/AppGestionMateriel.java
+++ src/com/ensa/gi4/AppGestionMateriel.java
@@ -15,7 +15,7 @@ public class AppGestionMateriel {
     public static void main(String[] args) {
         final GestionMaterielController gestionMaterielController = APPLICATION_CONTEXT.getBean(GestionMaterielController.class);
         while (true) { // pour que l'appliation tourne jusqu'à la demande de l'utilisateur de l'arrêter
-            gestionMaterielController.afficherMenu();
+           gestionMaterielController.afficherMenu();
         }
 
     }
