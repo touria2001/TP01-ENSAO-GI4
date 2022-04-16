399e87dbbc5990a8bab9f6a9704ff42a512be0aa
touria2001 <77863089+touria2001@users.noreply.github.com>
2022-04-16 01:54:27 +0000

    not working

--- src/com/ensa/gi4/AppGestionMateriel.java
+++ src/com/ensa/gi4/AppGestionMateriel.java
@@ -9,12 +9,15 @@ import com.ensa.gi4.modele.Livre;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SuppressWarnings("all")
+@Configuration
 @ComponentScan(basePackages = {"com.ensa.gi4"})
 public class AppGestionMateriel {
-    private static final ApplicationContext APPLICATION_CONTEXT;
+ //   private static final ApplicationContext APPLICATION_CONTEXT;
+	private static final AnnotationConfigApplicationContext  APPLICATION_CONTEXT ;
 
     static { // bloc static pour initilialisation
      //   APPLICATION_CONTEXT = new ClassPathXmlApplicationContext("/beans/*-context.xml");
@@ -24,8 +27,8 @@ public class AppGestionMateriel {
 
     public static void main(String[] args) {
         final GestionMaterielController gestionMaterielController = APPLICATION_CONTEXT.getBean(GestionMaterielController.class);
-        final ApplicationPublisher publisher = APPLICATION_CONTEXT.getBean(ApplicationPublisher.class);
-        publisher.publish(new MyEvent<>(new Livre(), EventType.ADD));
+      //  final ApplicationPublisher publisher = APPLICATION_CONTEXT.getBean(ApplicationPublisher.class);
+      //  publisher.publish(new MyEvent<>(new Livre(), EventType.ADD));
         
         while (true) { // pour que l'appliation tourne jusqu'à la demande de l'utilisateur de l'arrêter
             gestionMaterielController.afficherMenu();
